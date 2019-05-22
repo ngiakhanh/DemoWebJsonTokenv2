4d3fc76880becf78e5511867f4466f2bfca9843b
ngiakhanh <ngiakhanh96@gmail.com>
2019-05-22 14:13:44 +0700

    Initial commit

---
+++ src/JWTAPI/JWTAPI/Core/Security/Tokens/AccessToken.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace JWTAPI.Core.Security.Tokens
+{
+    public class AccessToken : JsonWebToken
+    {
+        public RefreshToken RefreshToken { get; private set; }
+
+        public AccessToken(string token, long expiration, RefreshToken refreshToken) : base(token, expiration)
+        {
+            RefreshToken = refreshToken ?? throw new ArgumentException("Specify a valid refresh token.");
+        }
+    }
+}
